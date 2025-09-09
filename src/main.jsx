f0f7a123d4335a0a927ca0b72ecea7f637ef9eab
felixriley <friley02@cgstudents.catholic.edu.au>
2025-09-09 16:02:36 +1000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import BatakHouseLanding from './BatakHouseLanding'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BatakHouseLanding />
+  </React.StrictMode>,
+)
