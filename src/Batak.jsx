165fd469f3f266abec6e770623c8194df0d56b8f
felixriley <friley02@cgstudents.catholic.edu.au>
2025-09-16 12:26:42 +1000

    Update Batak.jsx

--- src/Batak.jsx
+++ src/Batak.jsx
@@ -280,9 +280,9 @@ export default function Batak() {
                   allowFullScreen
                 />
               </div>
-            </motion.section
+            </motion.section>
               
-            {/* BIBLIOGRAPHY */>
+            {/* BIBLIOGRAPHY */}
             <motion.section
               ref={bibliographyRef}
               className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20"
